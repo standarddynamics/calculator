b56a5996f2de3f3694eb255a3c6e0e2fe4f37ce8
Kelvin Crabb <kelvin.crabb@icloud.com>
2025-02-19 19:56:32 +0000

    Adding more comments

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -124,19 +124,23 @@
         // Using a stack to manage operator precedence
         Stack<char> operatorStack = new();
         // Declaring operators as dictionary with their precedence
-        Dictionary<char, int> operators = new() 
+        Dictionary<char, int> operators = new()
         {
             { '+', 1 }, { '-', 1 }, { '*', 2 }, { '/', 2 }, { '^', 3 }
         };
+        // Declaring the evaluation as a stack - this will eventually be the result
+        Stack<double> evaluationStack = new();
 
+        // Converting expression string to reverse polish notation
         for (int i = 0; i < expression.Length; i++)
         {
             char ch = expression[i];
 
             if (char.IsDigit(ch))
             {
-                // Push the numbers to the queue
+                // Push the numbers to the outputQueue
                 string number = ch.ToString();
+                // While we're less than the length of the expression string and we're a digit, push to the end of queue
                 while (i + 1 < expression.Length && char.IsDigit(expression[i + 1]))
                 {
                     number += expression[++i];
@@ -145,6 +149,7 @@
             }
             else if (operators.TryGetValue(ch, out int value))
             {
+                // This is an operator so push to the end operator stack
                 // Ensure operators with higher or equal precedence are processed before the current operator
                 while (operatorStack.Count > 0 && operators.ContainsKey(operatorStack.Peek()) &&
                        operators[operatorStack.Peek()] >= value)
@@ -155,15 +160,21 @@
             }
             else if (ch == '(')
             {
+                // Opening parentheses must go to the front of the operator stack
                 operatorStack.Push(ch);
             }
             else if (ch == ')')
             {
+                // Closing parentheses 
+                // While there's items in the operator stack and we don't have any opening parentheses at the top of the stack
                 while (operatorStack.Count > 0 && operatorStack.Peek() != '(')
                 {
+                    // Making sure that operators are processed in the correct order between parentheses
+                    // So we're popping the first operator form the stack and adding them to the output queue to ensure their order
                     outputQueue.Enqueue(operatorStack.Pop().ToString());
                 }
 
+                // Check for mismatched parentheses
                 if (operatorStack.Count == 0 || operatorStack.Pop() != '(')
                 {
                     throw new Exception("Mismatched parentheses");
@@ -171,10 +182,12 @@
             }
             else
             {
+                // Just in case
                 throw new Exception($"Invalid character: {ch}");
             }
         }
 
+        // Handling operators after parentheses enqueueing them to the output queue
         while (operatorStack.Count > 0)
         {
             char op = operatorStack.Pop();
@@ -185,17 +198,20 @@
             outputQueue.Enqueue(op.ToString());
         }
 
-        Stack<double> evaluationStack = new();
-        
+        // Now to evaluate the reverse polish notation
+        // Iterating through the outputQueue
         while (outputQueue.Count > 0)
         {
+            // Get the first object in the queue
             string val = outputQueue.Dequeue();
+            // If it's a number, push it to the stack
             if (double.TryParse(val, out double number))
             {
                 evaluationStack.Push(number);
             }
             else
             {
+                // Must be a operator so we need to perform the correct operation on the next two items
                 double b = evaluationStack.Pop();
                 double a = evaluationStack.Pop();
 
